2e01f1a5c91306e740cda5291b7779cc76460e3f
Oatre <jensenandfur@gmail.com>
2023-06-19 01:06:48 +0200

    New stuff

--- src/Components/Herobanner/HeroBanner.jsx
+++ src/Components/Herobanner/HeroBanner.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from "../../Assets/whitenobackgrund.png";
 import './herobanner.css';
+import { Link, useLocation } from 'react-router-dom';
 
-export const HeroBanner = ({title}) => {
+export const HeroBanner = ({ title, link }) => {
+  const location = useLocation();
+
+const isHomepage = location.pathname === '/';
+
   return (
     <div className='heroWrapper'>
-      <img src={logo} alt="logo" style={{ maxWidth: '100%', height: 'auto' }} />
-      <h1>
-        {title}
-      </h1>
+      <Link to="/">
+        <img src={logo} alt="logo" style={{ maxWidth: '100%', height: 'auto' }} />
+      </Link>
+      <h1>{title}</h1>
+      {isHomepage && <h2>COACHING MED TO</h2>}
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,8 +7,11 @@ export const Home = () => {
   return (
     <div>
       <Navbar />
-      <HeroBanner title="HJEM" />
-      <h1>Testing update</h1>
+      <HeroBanner title="EVIDENCE-BASED COACHING DUO" />
+      <div className="content">
+        <p className="centered-paragraph">HJEM ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat eros id erat interdum dignissim. Donec sit amet faucibus est. Ut et maximus
+         felis. Morbi semper aliquam urna, at tempor risus laoreet sed. Sed nec luctus velit, vitae consectetur risus. Aenean non turpis eu purus tristique congue. Vestibulum pharetra arcu in odio placerat, non varius elit facilisis.</p>
+      </div>
     </div>
   );
 };
