36a55dca235133d61990ee6abff14d4bd9554944
Oatre <jensenandfur@gmail.com>
2023-06-30 12:58:58 +0200

    Fixed import so netlify can compile.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar } from '../Components/Navbar/Navbar';
 import './Home.css';
 import { HeroBanner } from '../Components/Herobanner/HeroBanner';
-import ImageWithText from '../Components/ImageWithText/ImageWithText'; //Can stay, may use later.
+// import ImageWithText from '../Components/ImageWithText/ImageWithText'; //Can stay, may use later.
 import PackagesSection from '../Components/Programpackages/PackagesSection';
 
 
