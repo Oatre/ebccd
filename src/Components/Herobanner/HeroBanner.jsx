5046aa96ef346675193f98c39282d2f054de404a
Oatre <jensenandfur@gmail.com>
2023-07-03 19:40:01 +0200

    Edited herobanner styling mostly.

--- src/Components/Herobanner/HeroBanner.jsx
+++ src/Components/Herobanner/HeroBanner.jsx
@@ -3,20 +3,26 @@ import logo from "../../Assets/whitenobackgrund.png";
 import './herobanner.css';
 import { Link, useLocation } from 'react-router-dom';
 
-
 export const HeroBanner = ({ title, link }) => {
   const location = useLocation();
-
-const isHomepage = location.pathname === '/';
+  const isHomepage = location.pathname === '/';
 
   return (
     <div className='heroWrapper'>
       <Link to="/">
         <img src={logo} alt="logo" style={{ maxWidth: '100%', height: 'auto' }} />
       </Link>
-      <h1>{title}</h1>
-      {isHomepage && <h2>SAMMEN ER VI STERKERE - MED TO COACHER, DOBLES RESULTATENE</h2>}
-      
+      <h1>
+        {title.split('').map((letter, index) => (
+          <span key={index} className={`letter-${index}`}>{letter}</span>
+        ))}
+      </h1>
+      {isHomepage && (
+        <div className="h2-wrapper">
+          <h2 className="h2-first">SAMMEN ER VI STERKERE</h2>
+          <h2 className="h2-second"> MED TO COACHER, DOBLES RESULTATENE</h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from '../Button/Button'; 
-import "../Navbar/navbar.css";
+import Button from '../Button/Button';
+import '../Navbar/navbar.css';
 
 export const Navbar = () => {
   return (
     <nav className="nav">
       <ul className="navbar">
-        <li>
+        <li className="navbar-item">
           <Link to="/">
             <Button label="HJEM" /> 
+            <div className="bar"></div>
           </Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/about">
             <Button label="OM OSS" /> 
+            <div className="bar"></div>
           </Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/coaching">
             <Button label="COACHING" />
+            <div className="bar"></div>
           </Link>
         </li>
       </ul>
