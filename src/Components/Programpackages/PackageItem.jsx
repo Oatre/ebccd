20d8cdf1667d9457cb309720e3992366f96a0ec4
David <davidaarlien@hotmail.com>
2023-07-07 00:55:33 +0200

    Added logic for reading packages data from json file, and fixed some styling

---
+++ src/Components/Programpackages/PackageItem.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import checkmark from '../../Assets/checkmark.png';
+import Button from '../Button/Button';
+
+const PackageItem = (item) => {
+
+  return (
+    <li className="card-list-item">
+        <div className="headline-wrapper">
+            <h2>{item.pack.title}</h2>
+            {
+            item.pack.titleIngress && 
+                <h3>{item.pack.titleIngress}</h3>
+            }
+            <p>{item.pack.subtitle}</p>
+        </div>
+        <div className="price-wrapper">
+            <h3>{item.pack.price}</h3>
+            <p>{item.pack.priceSubTitle}</p>
+        </div>
+        <ul className="info-list">
+        {
+            item.pack.benefits && item.pack.benefits.map((benefit) => (
+            <li className="info-item" key={benefit.id}>
+                <div className="dot">
+                {
+                    benefit.included ?
+                    <img src= {checkmark} alt="checkmark" />
+                    :
+                    <div className="dot" />
+                }
+                </div>
+                <p>{benefit.benefitTitle}</p>
+            </li>
+            ))
+        }
+        </ul>
+        <Button label= "Bestill"/>
+    </li>
+  )
+}
+
+export default PackageItem
